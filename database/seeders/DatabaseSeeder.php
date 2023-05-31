aa9041a826a7935c09fccc669bf1ba972c28575c
bayeng <bayurollins.87@gmail.com>
2023-05-31 10:28:45 +0700

    remove data seedder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,134 +24,134 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Tamu::create([
-            'name' => 'bayu1',
-            'email' => 'bayu@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-
-        Room::create([
-            'Pintu' => 5,
-            'RuangTamuDepan' => 5,
-            'RuangMakan' => 5,
-            'HalamanDepan' => 5,
-            'Lantai2' => 5,
-            'Balkon' => 5,
-            'Kamar' => 5,
-            'Toilet' => 5
-        ]);
-
-        Room::create([
-            'Pintu' => 5,
-            'RuangTamuDepan' => 5,
-            'RuangMakan' => 5,
-            'HalamanDepan' => 5,
-            'Lantai2' => 5,
-            'Balkon' => 5,
-            'Kamar' => 5,
-            'Toilet' => 5
-        ]);
-        // Time::create([
-        //     'Pintu'=> 3600,
-        //     'RuangTamuDepan'=> 3600,
-        //     'RuangMakan'=> 3600,
-        //     'HalamanDepan'=> 3600,
-        //     'Lantai2'=> 3600,
-        //     'Balkon'=> 3600,
-        //     'Kamar'=> 3600,
-        //     'Toilet'=> 3600
+        // Tamu::create([
+        //     'name' => 'bayu1',
+        //     'email' => 'bayu@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
         // ]);
-        Tamu::create([
-            'name' => 'danny1',
-            'email' => 'danny@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'fito1',
-            'email' => 'fito@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'murran1',
-            'email' => 'murran@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'bayu2',
-            'email' => 'bayu@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'danny2',
-            'email' => 'danny@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'fito2',
-            'email' => 'fito@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'murran2',
-            'email' => 'murran@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'bayu3',
-            'email' => 'bayu@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'danny3',
-            'email' => 'danny@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'fito3',
-            'email' => 'fito@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'murran3',
-            'email' => 'murran@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'bayu4',
-            'email' => 'bayu@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'danny4',
-            'email' => 'danny@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'fito4',
-            'email' => 'fito@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
-        Tamu::create([
-            'name' => 'murran4',
-            'email' => 'murran@gmail.com',
-            'nohp' => '089764567746',
-            'gender' => 'L'
-        ]);
+
+        // Room::create([
+        //     'Pintu' => 5,
+        //     'RuangTamuDepan' => 5,
+        //     'RuangMakan' => 5,
+        //     'HalamanDepan' => 5,
+        //     'Lantai2' => 5,
+        //     'Balkon' => 5,
+        //     'Kamar' => 5,
+        //     'Toilet' => 5
+        // ]);
+
+        // Room::create([
+        //     'Pintu' => 5,
+        //     'RuangTamuDepan' => 5,
+        //     'RuangMakan' => 5,
+        //     'HalamanDepan' => 5,
+        //     'Lantai2' => 5,
+        //     'Balkon' => 5,
+        //     'Kamar' => 5,
+        //     'Toilet' => 5
+        // ]);
+        // // Time::create([
+        // //     'Pintu'=> 3600,
+        // //     'RuangTamuDepan'=> 3600,
+        // //     'RuangMakan'=> 3600,
+        // //     'HalamanDepan'=> 3600,
+        // //     'Lantai2'=> 3600,
+        // //     'Balkon'=> 3600,
+        // //     'Kamar'=> 3600,
+        // //     'Toilet'=> 3600
+        // // ]);
+        // Tamu::create([
+        //     'name' => 'danny1',
+        //     'email' => 'danny@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'fito1',
+        //     'email' => 'fito@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'murran1',
+        //     'email' => 'murran@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'bayu2',
+        //     'email' => 'bayu@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'danny2',
+        //     'email' => 'danny@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'fito2',
+        //     'email' => 'fito@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'murran2',
+        //     'email' => 'murran@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'bayu3',
+        //     'email' => 'bayu@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'danny3',
+        //     'email' => 'danny@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'fito3',
+        //     'email' => 'fito@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'murran3',
+        //     'email' => 'murran@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'bayu4',
+        //     'email' => 'bayu@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'danny4',
+        //     'email' => 'danny@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'fito4',
+        //     'email' => 'fito@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
+        // Tamu::create([
+        //     'name' => 'murran4',
+        //     'email' => 'murran@gmail.com',
+        //     'nohp' => '089764567746',
+        //     'gender' => 'L'
+        // ]);
 
         User::create([
             'name' => 'summarecon',
